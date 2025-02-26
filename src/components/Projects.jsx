a252ae410a7417aed11deb5231f37b697ea0a6cc
Mohit Rana <mohitr8998@gmail.com>
2025-02-25 23:54:38 -0500

    added new projects in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import MidBox from "./MidBox";
 import TheDailyDrip from "../assets/TheDailyDrip.jpeg";
 import StackJourney from "../assets/StackJourney.jpeg";
+import Graphia from "../assets/Graphia.jpeg"
+import Logo100XGROW from "../assets/100XGROW.jpeg"
 import Title from "./Title";
 
 function Projects() {
@@ -12,12 +14,12 @@ function Projects() {
                     <Title TitleText={"Projects"} color={"#50DA4C"} />
                     <div className="h-full w-full border-y-4 border-dotted border-[#1C1C1C] my-8 py-4">
 
-                        {/* project 2 */}
+                        {/* project 1 */}
                         <div className="flex flex-col sm:flex-col justify-between items-center sm:gap-4 p-8 py-16 border-y-4 border-dotted border-[#1C1C1C] md:flex-col md:gap-4 lg:flex-row">
-                            <MidBox Picture={StackJourney} />
+                            <MidBox Picture={Logo100XGROW} />
                             <div className="w-full sm:w-[70%] text-[#1C1C1C] mt-4 sm:mt-0">
                             <div className="mt-4 flex flex-wrap gap-8">
-                                <a href="https://github.com/Mohitranag18/Stack-Journey"
+                                <a href="https://github.com/Mohitranag18/100XGROW"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     className="text-xl relative cursor-pointer group"
@@ -26,7 +28,7 @@ function Projects() {
                                     {"[ GitHub ]"}
                                 </a>
 
-                                <a href="https://github.com/Mohitranag18/Stack-Journey"
+                                <a href="https://github.com/Mohitranag18/100XGROW"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     className="text-xl relative cursor-pointer group"
@@ -35,9 +37,43 @@ function Projects() {
                                     {"[ Live Link ]"}
                                 </a>
                             </div>
-                                <h1 className="mt-4 text-3xl font-semibold">Stack Journey</h1>
+                                <h1 className="mt-4 text-3xl font-semibold">100XGROW</h1>
                                 <p className="mt-4">
-                                StackJourney is a blogging platform for developers and tech enthusiasts to share experiences, tips, and guidance. Users can create, read, edit, and delete posts, show appreciation for informative blogs, and manage personalized profiles. It's a space for beginners and experts alike to learn, share, and grow together.
+                                Comming Soon.....
+                                </p>
+                            </div>
+                            <div>
+                                {/* Hide [+] on mobile screens */}
+                                <p className="hidden lg:block text-4xl sm:text-5xl font-bold text-[#1C1C1C] hover:text-[#A600A0] transition-transform duration-500 ease-in-out hover:translate-y-[-10px] transform">[+]</p>
+                            </div>
+                        </div>
+
+                        {/* project 2 */}
+                        <div className="flex flex-col sm:flex-col justify-between items-center sm:gap-4 p-8 py-16 border-b-4 border-dotted border-[#1C1C1C] md:flex-col md:gap-4 lg:flex-row">
+                            <MidBox Picture={Graphia} />
+                            <div className="w-full sm:w-[70%] text-[#1C1C1C] mt-4 sm:mt-0">
+                            <div className="mt-4 flex flex-wrap gap-8">
+                                <a href="https://github.com/Mohitranag18/Graphia"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-xl relative cursor-pointer group"
+                                >
+                                    <span className="absolute ml-[10%] bottom-[-4px] left-1/2 w-0 h-[2px] bg-[#1C1C1C] transition-all duration-300 ease-out group-hover:w-[80%] group-hover:left-0"></span>
+                                    {"[ GitHub ]"}
+                                </a>
+
+                                <a href="https://github.com/Mohitranag18/Graphia"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-xl relative cursor-pointer group"
+                                >
+                                    <span className="absolute ml-[10%] bottom-[-4px] left-1/2 w-0 h-[2px] bg-[#1C1C1C] transition-all duration-300 ease-out group-hover:w-[80%] group-hover:left-0"></span>
+                                    {"[ Live Link ]"}
+                                </a>
+                            </div>
+                                <h1 className="mt-4 text-3xl font-semibold">Graphia</h1>
+                                <p className="mt-4">
+                                Graphia is a modern social media platform with features like CRUD operations, user profiles, JWT-based authentication, real-time private and group chats, file sharing, and advanced search. Built with React, Tailwind CSS, Django, and WebSockets, it plans to add audio and video calling using WebRTC.
                                 </p>
                             </div>
                             <div>
@@ -105,7 +141,7 @@ function Projects() {
                             </div>
                                 <h1 className="mt-4 text-3xl font-semibold">The Daily Drip</h1>
                                 <p className="mt-4">
-                                    The Daily Drip Website, A modern website for a vibrant cafe, offering food, drinks, games, and events. Features: -Online Food Ordering option -Event Notifications -Multichannel Contact(whatsapp, email, call) -Beautiful UI with Modern Animations(GSAP) -Built with HTML, CSS, JavaScript. Enhancing User Experience and Showcasing the Cafe's Unique Atmosphere.
+                                The Daily Drip Website, A modern website for a vibrant cafe, offering food, drinks, games, and event. Features: -Online Reservation Option -Event Notifications -Multichannel Contact(whatsapp, email, call) -Beautiful UI with Modern Animations(GSAP) -Built with HTML, CSS, JavaScript. Enhancing User Experience and Showcasing the Cafe.
                                 </p>
                             </div>
                             <div>
